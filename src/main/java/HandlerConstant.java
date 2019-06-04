7969640fd155982ec4db7e6db53f96ea8f2ed3a9
dlworldpeace <xdl_line@hotmail.com>
2019-06-04 13:03:48 +0800

    Reorganize the constants by putting all URL strings together.

--- src/main/java/HandlerConstant.java
+++ src/main/java/HandlerConstant.java
@@ -25,36 +25,31 @@ public class HandlerConstant {
 
   public static final String sslCertFilePath = "src/main/resources/key/deskera/deskera_ssl.p12";
   public static final String sslCertPwd = "sslpass";
-  public static final String proxyURL = ""; // what is this?
-  public static final String oAuthURL_UAT = "https://tts.sandbox.apib2b.citi.com/citiconnect/sb/authenticationservices/v1/oauth/token";
-  public static final String oAuthURL_PROD = "https://tts.apib2b.citi.com/citiconnect/prod/authenticationservices/v1/oauth/token";
+  public static final String proxyURL = ""; // what is this? Guess if it is intranet proxy
 
-  /* Sample Payment Initiation Payload */
+  /* URLs for both UAT and PROD */
 
-  public static final String samplePaymentPayload = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
-      + "<Request><paymentBase64>--Base64EncodedISOPaymentXML--</paymentBase64></Request>";
-
-  /* Inputs to the Payment Initiation API Calling Logic */
-
+  public static final String oAuthURL_UAT =
+      "https://tts.sandbox.apib2b.citi.com/citiconnect/sb/authenticationservices/"
+          + "v1/oauth/token";
+  public static final String oAuthURL_PROD =
+      "https://tts.apib2b.citi.com/citiconnect/prod/authenticationservices/v1/"
+          + "oauth/token";
   public static final String payInitURL_UAT =
       "https://tts.sandbox.apib2b.citi.com/citiconnect/sb/paymentservices/v1/"
           + "payment/initiation?";
   public static final String payInitURL_PROD =
       "https://tts.apib2b.citi.com/citiconnect/prod/paymentservices/v1/payment/"
           + "initiation?";
-
-  /* Inputs to the Balance Inquiry API Calling Logic */
-
   public static final String balanceInquiryUrl_UAT =
       "https://tts.sandbox.apib2b.citi.com/citiconnect/sb/accountstatementservices"
           + "/v1/balance/inquiry?";
   public static final String balanceInquiryUrl_PROD =
       "https://tts.apib2b.citi.com/citiconnect/prod/accountstatementservices/v1/"
           + "balance/inquiry?";
-
-  /* Inputs to the Statement Retrieval API Calling Logic */
-
-  public static final String statementRetUrl_UAT = "https://sit.api.citiconnect.citigroup.com/citidirect/uat/payments/v2/statement/retrieval";
+  public static final String statementRetUrl_UAT =
+      "https://tts.sandbox.apib2b.citi.com/citiconnect/sb/accountstatementservices/"
+          + "v1/statement/retrieval?";
   public static final String statementRetUrl_PROD =
       "https://tts.apib2b.citi.com/citiconnect/prod/accountstatementservices/v1/"
           + "statement/retrieval?";

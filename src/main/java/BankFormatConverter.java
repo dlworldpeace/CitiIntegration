a5c9c71caa1fadf0994ea443580de12647b637fe
dlworldpeace <xdl_line@hotmail.com>
2019-06-17 15:15:18 +0800

    Add new method to convert camt.053 document type to deskera statement type.

--- src/main/java/BankFormatConverter.java
+++ src/main/java/BankFormatConverter.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.bind.JAXBContext;
@@ -18,6 +19,7 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMResult;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import main.java.statement.DeskeraStatement;
 import org.eclipse.persistence.jaxb.JAXBContextProperties;
 
 /**
@@ -43,7 +45,7 @@ public class BankFormatConverter<T> {
    *
    * @param XMLStr XML String in standard ISO format.
    * @return a JAXBElement of a standard ISO format.
-   * @throws BankFormatConverterException if an unpexted event happens during
+   * @throws BankFormatConverterException if an unexpected event happens during
    *                                      the conversion.
    */
   public JAXBElement<T> readXMLToElement (String XMLStr)
@@ -65,9 +67,9 @@ public class BankFormatConverter<T> {
    *
    * @param rootElement JAXBElement rooted at Document Element.
    * @return corresponding XML String.
-   * @throws BankFormatConverterException if an unpexted event happens during the
-   *                                      conversion or adding some property to
-   *                                      the string styles.
+   * @throws BankFormatConverterException if an unexpected event happens during
+   *                                      the conversion or adding some property
+   *                                      to the string styles.
    */
   public String writeElementToXML (JAXBElement<T> rootElement)
       throws BankFormatConverterException {
@@ -98,7 +100,7 @@ public class BankFormatConverter<T> {
    *
    * @param rootElement JAXBElement rooted at Document Element.
    * @return corresponding JSON String.
-   * @throws BankFormatConverterException if an unpexted event happens during
+   * @throws BankFormatConverterException if an unexpected event happens during
    *                                      the conversion.
    */
   public String writeElementToJson (JAXBElement<T> rootElement)
@@ -118,6 +120,46 @@ public class BankFormatConverter<T> {
       Logger.getLogger(Handler.class.getName()).log(Level.SEVERE, null, e);
       throw new BankFormatConverterException(e.getMessage());
     }
+  }
+
+  public static JAXBElement<DeskeraStatement> convertCAMT053ToDeskeraStatement
+      (JAXBElement<deskera.fintech.camt053.Document> documentElement) {
+
+    DeskeraStatement deskeraStmt = new DeskeraStatement();
+    deskera.fintech.camt053.Document document = documentElement.getValue();
+
+    /* Start of Document */
+      /* Start of BkToCstmrStmt */
+    deskera.fintech.camt053.BankToCustomerStatementV02 bkToCstmrStmt =
+        document.getBkToCstmrStmt();
+        /* Start of GrpHdr */
+    deskera.fintech.camt053.GroupHeader42 grpHdr = bkToCstmrStmt.getGrpHdr();
+    deskeraStmt.setMsgId(grpHdr.getMsgId());
+        /* End of GrpHdr */
+        /* Start of Stmt */
+    List<deskera.fintech.camt053.AccountStatement2> stmtList =
+        bkToCstmrStmt.getStmt();
+    deskera.fintech.camt053.AccountStatement2 stmt = stmtList.get(0);
+    deskeraStmt.setStmtId(stmt.getId());
+    deskeraStmt.setCreDtTm(stmt.getCreDtTm());
+    deskeraStmt.setFrToDt(stmt.getFrToDt());
+    deskera.fintech.camt053.CashAccount20 acct = stmt.getAcct();
+    deskeraStmt.setAcctId(acct.getId().getOthr().getId());
+    deskeraStmt.setAcctNm(acct.getNm());
+    deskeraStmt.setAcctOwnrNm(acct.getOwnr().getNm());
+    deskeraStmt.setAcctSvcr(acct.getSvcr());
+    if (!stmt.getBal().isEmpty())
+      deskeraStmt.setBal(stmt.getBal());
+    if (!stmt.getIntrst().isEmpty())
+      deskeraStmt.setIntrst(stmt.getIntrst());
+    deskeraStmt.setTxsSummry(stmt.getTxsSummry());
+    if (!stmt.getNtry().isEmpty())
+      deskeraStmt.setNtry(stmt.getNtry());
+        /* End of Stmt */
+      /* End of BkToCstmrStmt */
+    /* End of Document */
+
+    return (new main.java.statement.ObjectFactory()).createStatement(deskeraStmt);
   }
 
 }

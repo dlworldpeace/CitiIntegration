57582bdc98258ab1d739b46ce0f8ef76e61d223f
dlworldpeace <xdl_line@hotmail.com>
2019-06-18 18:12:09 +0800

    Add method nextString to generatte a string of 8 characters

--- src/test/java/RandomStringGeneratorTest.java
+++ src/test/java/RandomStringGeneratorTest.java
@@ -1,9 +1,18 @@
 package test.java;
 
-import static org.junit.Assert.*;
-
 import junit.framework.TestCase;
+import main.java.RandomStringGenerator;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class RandomStringGeneratorTest extends TestCase {
+
+  @Test
+  public void nextString_success () {
+    RandomStringGenerator rsg = new RandomStringGenerator();
+    System.out.println(rsg.nextString());
+  }
 
 }

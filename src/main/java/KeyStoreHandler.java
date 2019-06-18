0c8b395e99be400e2e15f13c7a245694b54a94c6
dlworldpeace <xdl_line@hotmail.com>
2019-06-18 13:39:14 +0800

    Add both pos and neg unit tests for deleteP12IfExists method

--- src/main/java/KeyStoreHandler.java
+++ src/main/java/KeyStoreHandler.java
@@ -12,6 +12,7 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.security.GeneralSecurityException;
 import java.security.KeyFactory;
@@ -42,31 +43,31 @@ public class KeyStoreHandler {
    * Create a .p12 keystore file in the file system from a compatibble pair of
    * public cert and a private key
    *
-   * @param certificate {@link Path} to .crt file from input
-   * @param key {@link Path} to .key file from input
-   * @param keystore {@link Path} to the new .p12 keystore generated for output
+   * @param certificate Path to .crt file from input
+   * @param key Path to .key file from input
+   * @param keystore Path to the new .p12 keystore generated for output
    * @param password char array of password to protect the keystore generated
    * @throws KeyStoreHandlerException if an unexpected event occurs when loading
    *                                  input files from or writing new file into
    *                                  the file system, or if an unexpected event
    *                                  occurs during keys operations
    */
-  public static void createKeystoreFromCertAndKey(Path certificate, Path key, Path keystore,
-      char[] password) throws KeyStoreHandlerException {
+  public static void createKeystoreFromCertAndKey(String certificate, String key,
+      String keystore, char[] password) throws KeyStoreHandlerException {
     try {
-      byte[] pkcs8 = decode(Files.readAllBytes(key));
+      byte[] pkcs8 = decode(Files.readAllBytes(Paths.get(key)));
       KeyFactory kf = KeyFactory.getInstance("RSA");
       PrivateKey pvt = kf.generatePrivate(new PKCS8EncodedKeySpec(pkcs8));
       CertificateFactory cf = CertificateFactory.getInstance("X.509");
       Certificate pub;
-      try (InputStream is = Files.newInputStream(certificate)) {
+      try (InputStream is = Files.newInputStream(Paths.get(certificate))) {
         pub = cf.generateCertificate(is);
       }
       KeyStore pkcs12 = KeyStore.getInstance("PKCS12");
       pkcs12.load(null, null);
       pkcs12.setKeyEntry("identity", pvt, password, new Certificate[]{pub});
       try (OutputStream s = Files
-          .newOutputStream(keystore, StandardOpenOption.CREATE_NEW)) {
+          .newOutputStream(Paths.get(keystore), StandardOpenOption.CREATE_NEW)) {
         pkcs12.store(s, password);
       }
     } catch (IOException | GeneralSecurityException e) {
@@ -93,39 +94,30 @@ public class KeyStoreHandler {
   /**
    * Delete a .p12 extension key store file if it exists in the path specified
    *
-   * @param p12KeyStore {@link Path} to a specific .p12 keystore file
+   * @param p12KeyStore Path to a specific .p12 keystore file
    * @throws KeyStoreHandlerException if the file path specified does not end
    *                                  with .p12
    */
-  public static void deleteP12IfExist (Path p12KeyStore)
+  public static void deleteP12IfExists (String p12KeyStore)
       throws KeyStoreHandlerException {
-    try
-    {
+    try {
       PathMatcher matcher = FileSystems.getDefault()
-          .getPathMatcher("glob:*.p12");
-
-      if (!matcher.matches(p12KeyStore))
+          .getPathMatcher("glob:**/*.p12");
+      if (!matcher.matches(Paths.get(p12KeyStore)))
         throw new KeyStoreHandlerException("File path specified does not end with .p12");
-      Files.deleteIfExists(p12KeyStore);
-    }
-    catch(NoSuchFileException e)
-    {
-      Logger.getLogger(Handler.class.getName())
-          .log(Level.INFO, "No such file/directory exists");
-    }
-    catch(DirectoryNotEmptyException e)
-    {
-      Logger.getLogger(Handler.class.getName())
-          .log(Level.INFO, "Directory is not empty");
-    }
-    catch(IOException e)
-    {
-      Logger.getLogger(Handler.class.getName())
-          .log(Level.INFO, "Invalid permissions");
-    }
 
-    Logger.getLogger(Handler.class.getName())
-        .log(Level.INFO, "Delete .p12 successful");
+      boolean isDeleted = Files.deleteIfExists(Paths.get(p12KeyStore));
+      if (isDeleted)
+        Logger.getLogger(Handler.class.getName())
+            .log(Level.INFO, "Delete specified .p12 successful");
+      else
+        Logger.getLogger(Handler.class.getName())
+            .log(Level.INFO, "No such file/directory exists");
+    } catch(IOException e) {
+      Logger.getLogger(Handler.class.getName())
+          .log(Level.SEVERE, "Invalid permissions");
+      throw new KeyStoreHandlerException("Invalid permissions");
+    }
   }
 
 }
--- src/test/java/KeyStoreHandlerTest.java
+++ src/test/java/KeyStoreHandlerTest.java
@@ -1,7 +1,10 @@
 package test.java;
 
 import static main.java.KeyStoreHandler.createKeystoreFromCertAndKey;
+import static main.java.KeyStoreHandler.deleteP12IfExists;
 
+import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import junit.framework.TestCase;
 import main.java.KeyStoreHandlerException;
@@ -12,15 +15,46 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class KeyStoreHandlerTest extends TestCase {
 
+  private final String CERT_PATH =
+      "src/main/resources/key/deskera/deskera_sign_encryption_pubkey.crt";
+  private final String KEY_PATH =
+      "src/main/resources/key/deskera/deskera_customer_private.key";
+  private final String KS_PATH = "src/test/resources/key/as9Jijl4P2Yjhs.p12";
+  private final String NONEXSISTENT_KS_PATH = "src/test/resources/key/ha2dTaNfpOn.p12";
+  private final String FOLDER_PATH = "src/test/resources/key/";
+  private final String KS_PASSWORD = "7NLuioh2zn80";
+
   @Test
   public void createKeystoreFromCertAndKey_deskeraCrtAndPrivKey_success ()
       throws KeyStoreHandlerException {
 
+    deleteP12IfExists(KS_PATH);
     createKeystoreFromCertAndKey(
-        Paths.get("src/main/resources/key/deskera/deskera_sign_encryption_pubkey.crt"),
-        Paths.get("src/main/resources/key/deskera/deskera_customer_private.key"),
-        Paths.get("src/test/resources/key/new.p12"),
-        "123123123".toCharArray()
-    );
+        CERT_PATH, KEY_PATH, KS_PATH, KS_PASSWORD.toCharArray());
+  }
+
+  @Test
+  public void deleteP12IfExists_existingP12_deletionSuccess ()
+      throws KeyStoreHandlerException {
+
+    deleteP12IfExists(KS_PATH);
+    File f = new File(KS_PATH);
+    createKeystoreFromCertAndKey(
+        CERT_PATH, KEY_PATH, KS_PATH, KS_PASSWORD.toCharArray());
+    assertTrue(f.exists() && !f.isDirectory());
+    deleteP12IfExists(KS_PATH);
+    assertFalse(f.exists() && !f.isDirectory());
+  }
+
+  @Test
+  public void deleteP12IfExists_nonExistentP12_noDeletion ()
+      throws KeyStoreHandlerException {
+    deleteP12IfExists(NONEXSISTENT_KS_PATH);
+  }
+
+  @Test (expected = KeyStoreHandlerException.class)
+  public void deleteP12IfExists_nonFile_throwsException ()
+      throws KeyStoreHandlerException {
+    deleteP12IfExists(FOLDER_PATH);
   }
 }
